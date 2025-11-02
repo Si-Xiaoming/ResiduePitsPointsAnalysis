267c7c580f32be2bbab73ba172eb5adee71dc6c4
Si-Xiaoming <zbssm123@163.com>
2025-11-02 06:16:31 +0100

    feat: complete the whole sheet3 experiment.

---
+++ exp_sheet3/50_linear.sh
@@ -0,0 +1,10 @@
+
+CUDA_VISIBLE_DEVICES=0  # which gpu
+NUM_GPU=1
+CONFIG_PATH=configs/exp_sheet1/50_linear.py
+SAVE_PATH=/datasets/exp_sheet1/50_linear
+export CUDA_VISIBLE_DEVICES=${CUDA_VISIBLE_DEVICES}
+
+export PYTHONPATH=./   # Project path
+# python -c "import torch; print(torch.cuda.is_available());"
+python tools/train.py --config-file ${CONFIG_PATH} --num-gpus ${NUM_GPU} --options save_path=${SAVE_PATH}
